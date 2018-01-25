a7ed571cfbd4a3ece89769da6fb1914065197af3
cccczl <cccczl@hotmail.com>
2018-01-25 23:16:56 +0800

    shan

---
+++ testpy1/test15.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+list = []   ##建立空列表
+list.append('google')
+list.append('baidu')
+list.append('nihao')
+list.append('no')
+blist = [1,2,3,4]
+
+print list
+print "这个是最大值的元素", max(list)
+print "这个是最小值的元素", min(list)
+print list.pop(0)
+#print list.insert(1,'1111')
+print list.extend(blist)
+print list
+
