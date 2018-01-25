dfb69ea064febe8591657c2d2c251e378fc46396
cccczl <cccczl@hotmail.com>
2018-01-25 22:15:07 +0800

    shan

---
+++ testpy1/test14.py
@@ -0,0 +1,11 @@
+##!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+var1 = "Hello World!"
+var2 = "Python Runoob"
+
+print "var1[0]", var1[0]
+print "var2[1:5]",var2[1:5]
+
+print "更新字符串 :- ", var1[:6] + 'Runoob!'
+print "%s" %(var1[0])
