e871df4fbce283bbfc53361b7ef9ffd7fedccf93
cccczl <cccczl@hotmail.com>
2018-02-03 18:45:10 +0800

    测试

--- testpy1/test17.py
+++ testpy1/test17.py
@@ -3,4 +3,8 @@
 
 dict = {'Name': 'long', 'Age': 7, 'Class': 'First'}
 
-print "dict['Name']:", dict['Name'];
+print "dict['Name'r]:", dict['Name'];
+
+print dict.keys()
+print dict.values()
+print dict.items()
