2736eb7c1b20cbb71736ef0bb7b0bac76f518de9
cccczl <cccczl@hotmail.com>
2018-02-19 22:00:50 +0800

    kdkdk

---
+++ test19.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 定义函数
+def printme(str):
+    "打印任何传入的字符串"
+    print str;
+    return;
+
+
+printme("nihao")
+printme("这里传入文本参数给函数")
---
+++ test20.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 导入模块
+import support
+
+support.print_func("zhanglong")
